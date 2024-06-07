2b71fde751ed728b998d921af791efc930900a27
harianchery <hari.anchery001@gmail.com>
2024-06-07 16:33:55 +0530

    Data mining trial

--- src/components/ViewAll.jsx
+++ src/components/ViewAll.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const ViewAll = () => {
+    const [data, changeData] = useState(
+        [
+            { "name": "Aju", "admNo": 105, "rollNo": 15 },
+            { "name": "Arju", "admNo": 123, "rollNo": 10 },
+            { "name": "Ajith", "admNo": 114, "rollNo": 12 }
+        ]
+    )
+
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <center>
                 <h3><b>Registered Students Details</b></h3>
             </center>
@@ -13,125 +21,33 @@ const ViewAll = () => {
                 <div className="row">
                     <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                         <div className="row g-3">
-                            <div className=" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-                                <div className="card">
-                                    <img src="https://c8.alamy.com/comp/MMDF6C/one-young-school-girl-student-reading-book-study-education-in-class-MMDF6C.jpg" className="card-img-top" alt="..."></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title"><b>Student</b></h5>
+                            {data.map(
+                                (value, index) => {
+                                    return <div className=" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+                                        <div className="card">
+                                            <img src="https://c8.alamy.com/comp/MMDF6C/one-young-school-girl-student-reading-book-study-education-in-class-MMDF6C.jpg" className="card-img-top" alt="..."></img>
+                                            <div className="card-body">
+                                                <h5 className="card-title">Name:{value.name}</h5>
+                                                <h5 className="card-title">Adm No:{value.admNo}</h5>
+                                                <h5 className="card-title">Roll No:{value.rollNo}</h5>
 
-                                        <a href="#" className="btn btn-primary">View Profile</a>
+                                                <a href="#" className="btn btn-primary">View Profile</a>
+                                            </div>
+                                        </div>
+
+
                                     </div>
-                                </div>
+
+                                }
+                            )}
 
 
-                            </div>
-                            <div className=" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                <div className="card">
-                                    <img src="https://c8.alamy.com/comp/MMDF6C/one-young-school-girl-student-reading-book-study-education-in-class-MMDF6C.jpg" className="card-img-top" alt="..."></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title"><b>Student</b></h5>
 
-                                        <a href="#" className="btn btn-primary">View Profile</a>
-                                    </div>
-                                </div>
 
-                            </div>
-                            <div className=" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
 
-                                <div className="card">
-                                    <img src="https://c8.alamy.com/comp/MMDF6C/one-young-school-girl-student-reading-book-study-education-in-class-MMDF6C.jpg" className="card-img-top" alt="..."></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title"><b>Student</b></h5>
 
-                                        <a href="#" className="btn btn-primary">View Profile</a>
-                                    </div>
-                                </div>
 
-                            </div>
-                            <div className=" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div className="card">
-                                    <img src="https://c8.alamy.com/comp/MMDF6C/one-young-school-girl-student-reading-book-study-education-in-class-MMDF6C.jpg" className="card-img-top" alt="..."></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title"><b>Student</b></h5>
-
-                                        <a href="#" className="btn btn-primary">View Profile</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className=" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div className="card">
-                                    <img src="https://c8.alamy.com/comp/MMDF6C/one-young-school-girl-student-reading-book-study-education-in-class-MMDF6C.jpg" className="card-img-top" alt="..."></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title"><b>Student</b></h5>
-
-                                        <a href="#" className="btn btn-primary">View Profile</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className=" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div className="card">
-                                    <img src="https://c8.alamy.com/comp/MMDF6C/one-young-school-girl-student-reading-book-study-education-in-class-MMDF6C.jpg" className="card-img-top" alt="..."></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title"><b>Student</b></h5>
-
-                                        <a href="#" className="btn btn-primary">View Profile</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className=" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div className="card">
-                                    <img src="https://c8.alamy.com/comp/MMDF6C/one-young-school-girl-student-reading-book-study-education-in-class-MMDF6C.jpg" className="card-img-top" alt="..."></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title"><b>Student</b></h5>
-
-                                        <a href="#" className="btn btn-primary">View Profile</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className=" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div className="card">
-                                    <img src="https://c8.alamy.com/comp/MMDF6C/one-young-school-girl-student-reading-book-study-education-in-class-MMDF6C.jpg" className="card-img-top" alt="..."></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title"><b>Student</b></h5>
-
-                                        <a href="#" className="btn btn-primary">View Profile</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className=" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div className="card">
-                                    <img src="https://c8.alamy.com/comp/MMDF6C/one-young-school-girl-student-reading-book-study-education-in-class-MMDF6C.jpg" className="card-img-top" alt="..."></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title"><b>Student</b></h5>
-
-                                        <a href="#" className="btn btn-primary">View Profile</a>
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div className=" col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
-
-                                <div className="card">
-                                    <img src="https://c8.alamy.com/comp/MMDF6C/one-young-school-girl-student-reading-book-study-education-in-class-MMDF6C.jpg" className="card-img-top" alt="..."></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title"><b>Student</b></h5>
-                                        <a href="#" className="btn btn-primary">View Profile</a>
-                                    </div>
-                                </div>
-                            </div>
-                            
                         </div>
                     </div>
                 </div>
